fb0f17e3aabd94c813f8a246a03b3a12b52296ea
Junaid Rahim <junaidrahim8d@gmail.com>
2021-10-17 18:59:03 +0530

    font size change

--- hacken-fest/components/Home.jsx
+++ hacken-fest/components/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
     <VStack backgroundColor="peachgreen.50" justifyContent="space-around">
       <Img width="30%" src="/H-banner-light.svg"></Img>
       <Text fontSize="2xl" fontWeight="700" color="peachgreen.200">GDSC KIIT x IOT Lab</Text>
-      <Text fontSize="4xl" fontWeight="500" pt="3" pb="9">Open Source Contributors and Maintainers for 2021</Text>
+      <Text fontSize="3xl" fontWeight="500" pt="3" pb="9">Open Source Contributors and Maintainers for 2021</Text>
     </VStack>
   )
 }
