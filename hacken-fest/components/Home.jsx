ac9f231226b2b32e9edf8d0fe31f4cc3343d0025
Junaid Rahim <junaidrahim8d@gmail.com>
2021-10-17 20:31:37 +0530

    added mobile compatibility

--- hacken-fest/components/Home.jsx
+++ hacken-fest/components/Home.jsx
@@ -3,10 +3,10 @@ import { Img, Text } from "@chakra-ui/react"
 
 const Home = () => {
   return (
-    <VStack backgroundColor="peachgreen.50" justifyContent="space-around">
-      <Img width="30%" src="/H-banner-light.svg"></Img>
+    <VStack backgroundColor="peachgreen.50" justifyContent="center">
+      <Img width={{sm: "100%", md:"80%", lg: "35%"}} src="/H-banner-light.svg"></Img>
       <Text fontSize="2xl" fontWeight="700" color="peachgreen.200">GDSC KIIT x IOT Lab</Text>
-      <Text fontSize="3xl" fontWeight="500" pt="3" pb="9">Open Source Contributors and Maintainers for 2021</Text>
+      <Text align="center" fontSize="3xl" fontWeight="500" px="5" pt="3" pb="9">Open Source Contributors and Maintainers for 2021</Text>
     </VStack>
   )
 }
